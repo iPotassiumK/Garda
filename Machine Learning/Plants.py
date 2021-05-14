5ad5493d63a947ae23c96b669a613b48d0f9a43f
iPotassiumK <joekenking@ymail.com>
2021-05-14 16:16:18 +0700

    Modified the Plants.py script with more efficient code using loops.

--- Machine Learning/Plants.py
+++ Machine Learning/Plants.py
@@ -12,101 +12,56 @@ base_dir = "/home/potassium/Documents/Bangkit Capstone 2021/Dataset/"
 train_dir = os.path.join(base_dir, 'Train')
 validation_dir = os.path.join(base_dir, 'Validation')
 
-# Directory with our training pictures
-train_nightsade_dir = os.path.join(train_dir, 'Black Nightsade')
-train_broccoli_dir = os.path.join(train_dir, 'Broccoli')
-train_cabbage_dir = os.path.join(train_dir, 'Cabbage')
-train_cucumber_dir = os.path.join(train_dir, 'Cucumber')
-train_grape_dir = os.path.join(train_dir, 'Grape Vine')
-train_pepper_dir = os.path.join(train_dir, 'Pepper')
-train_potato_dir = os.path.join(train_dir, 'Potato')
-train_tomato_dir = os.path.join(train_dir, 'Tomato')
+class_list = ["Black Nightsade", "Broccoli", "Cabbage", "Cucumber", "Grape Vine", "Pepper", "Potato", "Tomato"]
 
-# Directory with our validation pictures
-validation_nightsade_dir = os.path.join(validation_dir, 'Black Nightsade')
-validation_broccoli_dir = os.path.join(validation_dir, 'Broccoli')
-validation_cabbage_dir = os.path.join(validation_dir, 'Cabbage')
-validation_cucumber_dir = os.path.join(validation_dir, 'Cucumber')
-validation_grape_dir = os.path.join(validation_dir, 'Grape Vine')
-validation_pepper_dir = os.path.join(validation_dir, 'Pepper')
-validation_potato_dir = os.path.join(validation_dir, 'Potato')
-validation_tomato_dir = os.path.join(validation_dir, 'Tomato')
+dictionary_class_dir = {}
 
-train_nightsade_fnames = os.listdir(train_nightsade_dir)
-train_broccoli_fnames = os.listdir(train_broccoli_dir)
-train_cabbage_fnames = os.listdir(train_cabbage_dir)
-train_cucumber_fnames = os.listdir(train_cucumber_dir)
-train_grape_fnames = os.listdir(train_grape_dir)
-train_pepper_fnames = os.listdir(train_pepper_dir)
-train_potato_fnames = os.listdir(train_potato_dir)
-train_tomato_fnames = os.listdir(train_tomato_dir)
-
-print('Total training Black Nightsade images :', len(os.listdir(train_nightsade_dir)))
-print('Total training Broccoli images :', len(os.listdir(train_broccoli_dir)))
-print('Total training Cabbage images :', len(os.listdir(train_cabbage_dir)))
-print('Total training Cucumber images :', len(os.listdir(train_cucumber_dir)))
-print('Total training Grape Vine images :', len(os.listdir(train_grape_dir)))
-print('Total training Pepper images :', len(os.listdir(train_pepper_dir)))
-print('Total training Potato images :', len(os.listdir(train_potato_dir)))
-print('Total training Tomato images :', len(os.listdir(train_tomato_dir)))
-
-print('Total validation Black Nightsade images :', len(os.listdir(validation_nightsade_dir)))
-print('Total validation Broccoli images :', len(os.listdir(validation_broccoli_dir)))
-print('Total validation Cabbage images :', len(os.listdir(validation_cabbage_dir)))
-print('Total validation Cucumber images :', len(os.listdir(validation_cucumber_dir)))
-print('Total validation Grape Vine images :', len(os.listdir(validation_grape_dir)))
-print('Total validation Pepper images :', len(os.listdir(validation_pepper_dir)))
-print('Total validation Potato images :', len(os.listdir(validation_potato_dir)))
-print('Total validation Tomato images :', len(os.listdir(validation_tomato_dir)))
+for model_class in class_list:
+    # Index 0 is Train directory, index 1 is Validation directory, index 2 is Train list
+    dictionary_class_dir[model_class] = []
+    dictionary_class_dir[model_class].append(os.path.join(train_dir, model_class))
+    dictionary_class_dir[model_class].append(os.path.join(validation_dir, model_class))
+    dictionary_class_dir[model_class].append(os.listdir(dictionary_class_dir[model_class][0]))
+    print("Total training {} images : {}".format(model_class, len(dictionary_class_dir[model_class][2])))
+    print("Total validating {} images : {}".format(model_class, len(os.listdir(dictionary_class_dir[model_class][1]))))
+    print("")
+    
+#print(dictionary_class_dir["Broccoli"][0])
+#print(dictionary_class_dir["Cabbage"][1])
+#print(dictionary_class_dir["Broccoli"][2][:5])
 
 import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
+import random
 
 # Parameters for our graph; we'll output images in a 4x4 configuration
-nrows = 8
+nrows = len(class_list)
 ncols = 4
 
-pic_index = 0 # Index for iterating over images
+pic_index = 4 # Index for iterating over images
 
 # Set up matplotlib fig, and size it to fit 4x4 pics
 fig = plt.gcf()
 fig.set_size_inches(ncols*4, nrows*4)
 
-pic_index+=4
+next_pix = []
+#all_pix = []
 
-next_nightsade_pix = [os.path.join(train_nightsade_dir, fname) 
-                for fname in train_nightsade_fnames[ pic_index-4:pic_index] 
-               ]
+for model_class in class_list:
+    for i in range(pic_index):
+        next_pix.append(os.path.join(dictionary_class_dir[model_class][0], random.choice(dictionary_class_dir[model_class][2])))
 
-next_broccoli_pix = [os.path.join(train_broccoli_dir, fname) 
-                for fname in train_broccoli_fnames[ pic_index-4:pic_index] 
-               ]
-               
-next_cabbage_pix = [os.path.join(train_cabbage_dir, fname) 
-                for fname in train_cabbage_fnames[ pic_index-4:pic_index] 
-               ]
+#print(next_pix)   
 
-next_cucumber_pix = [os.path.join(train_cucumber_dir, fname) 
-                for fname in train_cucumber_fnames[ pic_index-4:pic_index] 
-               ]
+"""
+for i in range(len(next_pix)):
+    for picture_path in next_pix[i]:
+        all_pix.append(picture_path)
 
-next_grape_pix = [os.path.join(train_grape_dir, fname) 
-                for fname in train_grape_fnames[ pic_index-4:pic_index] 
-               ]
+#print(all_pix)
+"""
 
-next_pepper_pix = [os.path.join(train_pepper_dir, fname) 
-                for fname in train_pepper_fnames[ pic_index-4:pic_index] 
-               ]
-
-next_potato_pix = [os.path.join(train_potato_dir, fname) 
-                for fname in train_potato_fnames[ pic_index-4:pic_index] 
-               ]
-               
-next_tomato_pix = [os.path.join(train_tomato_dir, fname) 
-                for fname in train_tomato_fnames[ pic_index-4:pic_index] 
-               ]
-               
-for i, img_path in enumerate(next_nightsade_pix+next_broccoli_pix+next_cabbage_pix+next_cucumber_pix+next_grape_pix+next_pepper_pix+next_potato_pix+next_tomato_pix):
+for i, img_path in enumerate(next_pix):
     # Set up subplot; subplot indices start at 1
     sp = plt.subplot(nrows, ncols, i + 1)
     sp.axis('Off') # Don't show axes (or gridlines)
@@ -173,6 +128,7 @@ model.summary()
 
 model.compile(loss = 'categorical_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
 
+
 history = model.fit(train_generator, epochs=25, steps_per_epoch=40, validation_data = validation_generator, verbose = 1, validation_steps=4)
 
 model.save("Plants.h5")
@@ -191,6 +147,5 @@ plt.plot(epochs, val_acc, 'b', label='Validation accuracy')
 plt.title('Training and validation accuracy')
 plt.legend(loc=0)
 plt.figure()
-
 
 plt.show()
