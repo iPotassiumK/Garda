3bc7f689f072423a262d0d3d19461d83563c9fef
monifaar <monifacool78@gmail.com>
2021-05-18 21:07:18 +0700

    change splash screen

--- Mobile Development/Garda/app/src/main/java/com/example/garda/SplashScreen.kt
+++ Mobile Development/Garda/app/src/main/java/com/example/garda/SplashScreen.kt
@@ -1,20 +1,28 @@
 package com.example.garda
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_splash_screen.*
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
 
 class SplashScreen : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
 
-        img_screen.alpha=0f
-        img_screen.animate().setDuration(100).alpha(1f).withEndAction{
+
+        Handler(Looper.getMainLooper()).postDelayed({
+
+
             val n = Intent(this, MainActivity::class.java)
             startActivity(n)
             finish()
-        }
+
+        }, 4500)
+
+
     }
 }
