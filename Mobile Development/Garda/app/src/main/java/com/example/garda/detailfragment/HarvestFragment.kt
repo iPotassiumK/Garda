aeb30483da200f7b96f9690a0aeaf7c4d9519736
adilahayoeb <adilahayoeb60@gmail.com>
2021-05-18 18:04:44 +0700

    first program

---
+++ Mobile Development/Garda/app/src/main/java/com/example/garda/detailfragment/HarvestFragment.kt
@@ -0,0 +1,23 @@
+package com.example.garda.detailfragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.garda.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+
+class HarvestFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_harvest,container, false)
+    }
+}
---
+++ Mobile Development/Garda/app/src/main/java/com/example/garda/fragment/SearchFragment.kt
@@ -0,0 +1,19 @@
+package com.example.garda.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.garda.R
+
+class SearchFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_search, container, false)
+    }
+}
