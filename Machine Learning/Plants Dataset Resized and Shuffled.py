1c0d5dbc3abd54159ed3e035426fffafc0ab2a72
iPotassiumK <joekenking@ymail.com>
2021-05-20 20:00:30 +0700

    Modified Python script: Plants Dataset Resized and Shuffled.py. With the following changes:     Added test directory for iamges to be shuffled and moved automatically to, with a defined split size.     Added class for the function to tell the class for file copying.     Changed quantity for 3 folder: Training, Validation, and Test with split size:         Training = Total images * 0.8         Validation = (Total images - Training) * 0.8         Test = Total images - Training - Validation
    For local use only.

--- Machine Learning/Plants Dataset Resized and Shuffled.py
+++ Machine Learning/Plants Dataset Resized and Shuffled.py
@@ -6,45 +6,64 @@ import shutil
 import math
 from PIL import Image
 
-def split_and_resize_data(SOURCE, TRAINING, VALIDATION, SPLIT_SIZE):
+def split_and_resize_data(CLASS, SOURCE, TRAINING, VALIDATION, TEST, SPLIT_SIZE):
     #Quantity set
     size = 150, 150
     counter = 0
     training_quantity = math.floor(len(os.listdir(SOURCE)) * SPLIT_SIZE)
-    validation_quantity = len(os.listdir(SOURCE)) - training_quantity
+    validation_quantity = math.floor((len(os.listdir(SOURCE)) - training_quantity)* SPLIT_SIZE)
+    test_quantity = len(os.listdir(SOURCE)) - training_quantity - validation_quantity
     
     #Random picker
     random_pick = random.sample(os.listdir(SOURCE), len(os.listdir(SOURCE)))
 
     #Copying files
-    #for i in range(len(random_pick)):
-    for i in range(training_quantity + validation_quantity):
+    for i in range(len(random_pick)):
+    #for i in range(training_quantity + validation_quantity):
         if i < training_quantity:
             im = Image.open(SOURCE + "/" + random_pick[i])
-            im.thumbnail(size, Image.ANTIALIAS)
-            im.save(TRAINING + "/" + random_pick[i], "JPEG")
+            try:
+                im.thumbnail(size, Image.ANTIALIAS)
+                im.save(TRAINING + "/" + random_pick[i], "JPEG")
+            except OSError:
+                print("Failed to modify image at : {}".format(SOURCE + "/" + random_pick[i]))
             #copyfile(SOURCE + random_pick[i], TRAINING + random_pick[i])
             #print("Copied " + random_pick[i] + " to " + TRAINING)
             #pass
-        else:
+        elif i < (validation_quantity + training_quantity):
             im = Image.open(SOURCE + "/" + random_pick[i])
-            im.thumbnail(size, Image.ANTIALIAS)
-            im.save(VALIDATION + "/" + random_pick[i], "JPEG")
+            try:
+                im.thumbnail(size, Image.ANTIALIAS)
+                im.save(VALIDATION + "/" + random_pick[i], "JPEG")
+            except OSError:
+                print("Failed to modify image at : {}".format(SOURCE + "/" + random_pick[i]))
             #copyfile(SOURCE + random_pick[i], VALIDATION + random_pick[i])
             #print("Copied " + random_pick[i] + " to " + VALIDATION)
             #pass
+        else:
+            im = Image.open(SOURCE + "/" + random_pick[i])
+            try:
+                im.thumbnail(size, Image.ANTIALIAS)
+                im.save(TEST + "/" + random_pick[i], "JPEG")
+            except OSError:
+                print("Failed to modify image at : {}".format(SOURCE + "/" + random_pick[i]))
+            #copyfile(SOURCE + random_pick[i], TEST + random_pick[i])
+            #print("Copied " + random_pick[i] + " to " + TEST)
+            #pass
+            
         counter+=1
 
-    print("Copied {} files".format(counter))
+    print("Copied {} files with {} training, {} validating, {} testing for class {}".format(counter, training_quantity, validation_quantity, test_quantity, CLASS))
 
-split_size = .9
+split_size = .8
 
 base_dir = "/home/potassium/Documents/Bangkit Capstone 2021/Dataset/"
-source_dir = "/run/media/potassium/Local-Disk_E/Homework/6th Semester/Bangkit 2021/Capstone/Dataset/"
+source_dir = "/run/media/potassium/Local-Disk_E/Homework/6th Semester/Bangkit 2021/Capstone/Dataset/Google Images/"
 
 # Directory for train and validation
 train_dir = os.path.join(base_dir, 'Train')
 validation_dir = os.path.join(base_dir, 'Validation')
+test_dir = os.path.join(base_dir, 'Test')
 
 class_list = ["Black Nightshade", "Broccoli", "Cabbage", "Cucumber", "Grape Vine", "Pepper", "Potato", "Tomato"]
 
@@ -67,12 +86,13 @@ except OSError:
     print("Directory already exist!")
 
 for model_class in class_list:
-    # Index 0 is Train directory, index 1 is Validation directory, index 2 is Source directory
+    # Index 0 is Train directory, index 1 is Validation directory, index 2 is Test directory, index 3 is Source directory
     dictionary_class_dir[model_class] = []
     dictionary_class_dir[model_class].append(os.path.join(train_dir, model_class))
     dictionary_class_dir[model_class].append(os.path.join(validation_dir, model_class))
+    dictionary_class_dir[model_class].append(os.path.join(test_dir, model_class))
     dictionary_class_dir[model_class].append(os.path.join(source_dir, model_class))
-    split_and_resize_data(dictionary_class_dir[model_class][2], dictionary_class_dir[model_class][0], dictionary_class_dir[model_class][1], split_size)
+    split_and_resize_data(model_class, dictionary_class_dir[model_class][3], dictionary_class_dir[model_class][0], dictionary_class_dir[model_class][1], dictionary_class_dir[model_class][2], split_size)
 
 #print(dictionary_class_dir["Broccoli"][0])
 #print(dictionary_class_dir["Cabbage"][1])
