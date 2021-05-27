57934acebd2c8badc58a6a0a02bfb6faeca7cf67
monifaar <monifacool78@gmail.com>
2021-05-27 23:13:00 +0700

    Add some changes from ML code

--- Mobile Development/Garda/app/src/main/java/com/example/garda/FirebaseStorageManager.kt
+++ Mobile Development/Garda/app/src/main/java/com/example/garda/FirebaseStorageManager.kt
@@ -7,7 +7,7 @@ import android.util.Log
 import com.google.firebase.storage.FirebaseStorage
 
 class FirebaseStorageManager {
-    private val TAG = "FirebaseStorageManager "
+    private val TAG = "FirebaseStorageManager"
     private val mStorageRef = FirebaseStorage.getInstance().reference
     private lateinit var mProgressDialog: ProgressDialog
 
@@ -16,7 +16,7 @@ class FirebaseStorageManager {
             mProgressDialog = ProgressDialog(mContext)
             mProgressDialog.setMessage("Please wait, image being uploading...")
 
-            val uploadTask = mStorageRef.child("users/profilePic.png").putFile(imageURI)
+            val uploadTask = mStorageRef.child("users/profilePic.png"+ imageURI.getLastPathSegment()).putFile(imageURI)
 
             uploadTask.addOnSuccessListener {
                 //success
