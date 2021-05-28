045b9cd47390b524a1e313ccd5a7e23c3321decc
adilahayoeb <adilahayoeb60@gmail.com>
2021-05-28 20:34:28 +0700

    "add onboarding screen"

---
+++ Mobile Development/Garda/app/src/main/java/com/example/garda/onboarding/OnBoardingData.kt
@@ -0,0 +1,3 @@
+package com.example.garda.onboarding
+
+class OnBoardingData (var title:String, var description:String, var imageUrl: Int)
---
+++ Mobile Development/Garda/app/src/main/java/com/example/garda/onboarding/OnBoardingViewPagerAdapter.kt
@@ -0,0 +1,47 @@
+package com.example.garda.onboarding
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.viewpager.widget.PagerAdapter
+import com.example.garda.R
+
+class OnBoardingViewPagerAdapter(private var context: Context, private var onBoardingDataList: List <OnBoardingData>) :
+    PagerAdapter() {
+
+    override fun isViewFromObject(view: View, `object`: Any): Boolean {
+        return view == `object`
+    }
+
+    override fun getCount(): Int {
+        return onBoardingDataList.size
+    }
+
+    override fun destroyItem(container: ViewGroup, position: Int, `object`: Any) {
+
+        container.removeView(`object` as View)
+
+    }
+
+    override fun instantiateItem(container: ViewGroup, position: Int): Any {
+        val view = LayoutInflater.from(context).inflate(R.layout.onboarding_screen_layout, null);
+        val imageView: ImageView
+        val title: TextView
+        val description : TextView
+
+        imageView = view.findViewById(R.id.imageView);
+        title = view.findViewById(R.id.title);
+        description = view.findViewById(R.id.description);
+
+        imageView.setImageResource(onBoardingDataList[position].imageUrl)
+        title.text = onBoardingDataList[position].title
+        description.text = onBoardingDataList[position].description
+
+        container.addView(view)
+        return view
+
+    }
+}
