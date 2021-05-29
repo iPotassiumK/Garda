ac3b02ed887e43c5d9d7200dcf553b1865636fc9
monifaar <monifacool78@gmail.com>
2021-05-29 10:20:44 +0700

    Added change DetailBlogActivity and layout

--- Mobile Development/Garda/app/src/main/java/com/example/garda/detailblog/DetailBlogActivity.kt
+++ Mobile Development/Garda/app/src/main/java/com/example/garda/detailblog/DetailBlogActivity.kt
@@ -11,15 +11,20 @@ class DetailBlogActivity : AppCompatActivity() {
     companion object {
         const val EXTRA_BLOG = "extra_blog"
     }
+    val blogs
+        get() = intent.extras?.getParcelable<BlogEntity>(EXTRA_BLOG)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail_blog)
 
+
         var nameTitle:TextView = findViewById(R.id.txt_name_title)
         var nameFull:TextView = findViewById(R.id.txt_name_full)
-        var imageBlog: ImageView = findViewById(R.id.imageView3)
+        var imageBlog: ImageView = findViewById(R.id.imageBlog)
 
-        nameTitle.text =
+        nameTitle.text = blogs?.nameTitle
+        nameFull.text = blogs?.nameFull
+        imageBlog.setImageResource(blogs!!.imageBlog)
     }
 }
