083acf9aedc6b1414caaa6997de5a72812b50d0d
adilahayoeb <adilahayoeb60@gmail.com>
2021-05-31 19:33:28 +0700

    "add firebasefirestore code"

--- Mobile Development/Garda/app/src/main/java/com/example/garda/DetailActivity.kt
+++ Mobile Development/Garda/app/src/main/java/com/example/garda/DetailActivity.kt
@@ -3,8 +3,10 @@ package com.example.garda
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.ImageButton
+import com.google.firebase.firestore.FirebaseFirestore
 
 class DetailActivity : AppCompatActivity(), View.OnClickListener {
 
@@ -12,9 +14,29 @@ class DetailActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var ic_camera : ImageButton
     private lateinit var ic_search : ImageButton
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
+
+        //get data from firebasefirestore
+        val db = FirebaseFirestore.getInstance()
+
+        val docRef = db.collection("plants").document("grape vine")
+        docRef.get()
+            .addOnSuccessListener{ document ->
+                if(document != null) {
+                    Log.d("yeayy exist", "DocumentSnapshot data: ${document.data}")
+                } else {
+                    Log.d("sorry there is no exist", "No Such Document")
+                }
+            }
+            .addOnFailureListener {exception ->
+                Log.d("sorry you get an error", "get failed with", exception)
+            }
+
+
+
 
         ic_home = findViewById(R.id.ic_home)
         ic_home.setOnClickListener(this)
