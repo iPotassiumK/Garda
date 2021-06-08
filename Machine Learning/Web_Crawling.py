0191c419a4519087074b4725bb9ed35740eae64d
deanpassaddhi10@example.com <deanpassaddhi10@example.com>
2021-06-08 13:44:16 +0700

    Adding list journals plants

--- Machine Learning/Web_Crawling.py
+++ Machine Learning/Web_Crawling.py
@@ -4,15 +4,15 @@ from bs4 import BeautifulSoup as soup
 import re
 import pandas as pd
 
-plant = urlopen('https://en.wikipedia.org/wiki/Potato') #change Potato with Tomato, Carrot, Spinach, Chili Pepper, Onion, Vitis/Grape, Cabbage, Broccoli, Cucumber
+plant = urlopen('https://en.wikipedia.org/wiki/Strawberry') #change Potato with other plants
 bsobj = soup(plant.read(), features = 'lxml')
 
 data = []
-for link in bsobj.find('div', {'id':'bodyContent'}).findAll('a', href = re.compile('^(https://)((?!:).)*$')):
+for link in bsobj.find('div', {'id':'bodyContent'}).findAll('a', href = re.compile('^(https://doi)((?!:).)*$')):
   if 'href' in link.attrs:
 #    print(link.attrs['href'])
     data.append({'url':link.attrs['href']})
 
-df = pd.DataFrame(data[5:15])
-df.to_csv('potato.csv', index = 'False', encoding = 'utf-8') #change file name with corresponding plant name above
+df = pd.DataFrame(data)
+df.to_csv('strawberry.csv', index = 'False', encoding = 'utf-8') #change file name with corresponding plant name above
 
