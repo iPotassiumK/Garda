5cf16188917a7d4c4bbe972a49eb54cee2517e16
monifaar <monifacool78@gmail.com>
2021-06-08 12:27:49 +0700

    added

--- Mobile Development/Garda/app/src/main/java/com/example/garda/viewpager/SectionPagerAdapter.kt
+++ Mobile Development/Garda/app/src/main/java/com/example/garda/viewpager/SectionPagerAdapter.kt
@@ -9,6 +9,7 @@ import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.example.garda.detailfragment.AboutFragment
 import com.example.garda.detailfragment.HarvestFragment
 import com.example.garda.detailfragment.InstructionFragment
+import com.example.garda.detailfragment.JournalFragment
 
 class SectionPagerAdapter(activity: AppCompatActivity, data:Bundle) : FragmentStateAdapter(activity)  {
 
@@ -20,12 +21,13 @@ class SectionPagerAdapter(activity: AppCompatActivity, data:Bundle) : FragmentSt
             0 -> fragment = AboutFragment()
             1 -> fragment = InstructionFragment()
             2 -> fragment = HarvestFragment()
+            3 -> fragment = JournalFragment()
         }
         fragment?.arguments=this.fragmentBundle
         return fragment as Fragment
     }
 
     override fun getItemCount(): Int {
-        return 3
+        return 4
     }
 }
